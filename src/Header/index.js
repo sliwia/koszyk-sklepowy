7a1d9ea5b728f7f823db18b33385035b53954dd6
liwia_szajner <liwia.szajner@gmail.com>
2019-08-13 21:09:39 +0200

    Zmiana klasy na funkcje w komponencie

--- src/Header/index.js
+++ src/Header/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import './Header.css';
 import '../MainCssFiles/Container.css'
 
-export class HeaderView extends React.Component {
-    render() {
-  
-      return (
-        <div className ="header-container">
-          <div className="container titles-container">
-              <h1 >Zrób zakupy !</h1>
-              <h2>Sprawdź co masz w koszyku:</h2>
-          </div>
-        </div>
-      );
-    }
+export function HeaderView() {
+  return (
+    <div className ="header-container">
+      <div className="container titles-container">
+          <h1 >Zrób zakupy !</h1>
+          <h2>Sprawdź co masz w koszyku:</h2>
+      </div>
+    </div>
+  );
   }

71ec3668922472b7a4b4886c77ddcb7b7b18429f
Tomasz Nowak <tn@mpro.pl>
2019-09-18 19:34:58 +0200

    Dodanie product list

--- src/SubMenuContainer/LogoContainer/LogoContainer.js
+++ src/SubMenuContainer/LogoContainer/LogoContainer.js
@@ -5,11 +5,9 @@ import Logo from "../../files/groceries.png";
 export const LogoContainer = () => {
   return (
     <>
-<<<<<<< HEAD
-      <div class='logo-container'><img src={Logo} width='100' height='100' alt='website logo' /></div>
-=======
-        <div class="logo-container"><img src={ Logo } width="100" height="100" alt="website logo" /></div>     
->>>>>>> 1671a72d3e1ad78ae8a53c72d4bd4cb44ad51d46
+      <div className='logo-container'>
+        <img src={Logo} width='100' height='100' alt='website logo' />
+      </div>
     </>
   );
 };

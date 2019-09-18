2f4c07e59f4457b08f2c1bfb1a353f23e5566741
Tomasz Nowak <tn@mpro.pl>
2019-09-18 19:21:57 +0200

    poprawka nazywy klas i dodanie height

--- src/SubMenuContainer/LogoContainer/LogoContainer.js
+++ src/SubMenuContainer/LogoContainer/LogoContainer.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import './LogoContainer.scss';
-import Logo from '../../files/groceries.png';
-
+import React from "react";
+import "./LogoContainer.scss";
+import Logo from "../../files/groceries.png";
 
 export const LogoContainer = () => {
-    return (
+  return (
     <>
-        <div class="logo-container"><img src={ Logo } width="100" width="100" alt="website logo" /></div>     
+      <div className='logo-container'>
+        <img src={Logo} width='100' height='100' alt='website logo' />
+      </div>
     </>
-    );
-}
+  );
+};

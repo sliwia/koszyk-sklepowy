2617bea1b3cbe6831ec6e3c08c9d23f62dbba692
Tomasz Nowak <tn@mpro.pl>
2019-09-19 08:57:33 +0200

    poprawki w stylowaniu listy produktó

--- src/AddElementToShoppingList/AddElementToShoppingList.js
+++ src/AddElementToShoppingList/AddElementToShoppingList.js
@@ -1,28 +1,22 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import './AddElementToShoppingList.scss';
-
+import React from "react";
+import "./AddElementToShoppingList.scss";
 
 export class AddElementToShoppingList extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  //constructor(props) {
+  //  super(props);
+  //}
 
-    render() {
-        const liStyle = {backgroundImage: 'url(' + this.props.photo + ')'};
-        return(
-        <div className="grid-container--item">
-            <div className="grid-container--item--elements">
-                <div className="product-img" style={liStyle} ></div>
-                <span>{this.props.product}</span>
-                <span>Cena: {this.props.price}</span>
-                <button className="btn-add-product">
-                    <FontAwesomeIcon icon={ faPlus } />
-                </button>
-            </div>
+  render() {
+    const liStyle = { backgroundImage: "url(" + this.props.photo + ")" };
+    return (
+      <div className='grid-container--item'>
+        <div className='grid-container--item--elements'>
+          <div className='product-img' style={liStyle}></div>
+          <span>{this.props.product}</span>
+          <span>Cena: {this.props.price}</span>
+          <div className='btn-green'>dodaj do koszyka</div>
         </div>
-        );
-    }
-
+      </div>
+    );
+  }
 }
